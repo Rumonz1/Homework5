382fcd131f6ecedc2408023259b2fb1016041339
Rumonz <lone_assassin@mail.ru>
2022-10-30 02:08:34 +0300

    Выполнил задание 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        byte clientOS = 1;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+        }
+        //Задание 2
+
+    }
+}
