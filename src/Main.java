ed0c1f5459bcc849951e1481ea52dd49ba5b381d
Rumonz <lone_assassin@mail.ru>
2022-10-30 02:35:25 +0300

    Выполнил задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         byte clientOS = 1;
-        switch (clientOS){
+        switch (clientOS) {
             case 0:
                 System.out.println("Установите версию приложения для iOS по ссылке");
                 break;
@@ -11,6 +11,19 @@ public class Main {
                 break;
         }
         //Задание 2
+        int clientDeviceYear = 2015;
+        byte operationSystem = 1;
+        if (clientDeviceYear >= 2015&& clientDeviceYear <= 2022 && operationSystem == 1 ) {
+            System.out.println("Установите приложение на ваше Android устройство!");
+        } else if (clientDeviceYear >= 2015&& clientDeviceYear <= 2022 && operationSystem == 0) {
+            System.out.println("Установите приложение на ваше iOS устройство!");
+        }
+        if (clientDeviceYear < 2015 && operationSystem == 1){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else if (clientDeviceYear < 2015 && operationSystem == 0){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        //Задание 3
 
     }
 }
