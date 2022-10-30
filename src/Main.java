d4b10902b6c8c024a458fcfd5d15aaa392e75eb8
Rumonz <lone_assassin@mail.ru>
2022-10-30 03:23:31 +0300

    не Выполнил задание 4, буду ждать видоразбор, выполнил задание 5

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,32 @@ public class Main {
         }else if (year % 100 !=0){
             System.out.println(+year + " год не является високосным");
         }
-        //Задание 4
+        //Задание 4 - жэу видеоразбора
+        //Задание 5
+        byte monthNumber = 12;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зимний сезон");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весенний сезон");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Летний сезон");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осенний сезон");
+                break;
+            default:
+                System.out.println("Такого месяца не существует!");
+        }
     }
 }
